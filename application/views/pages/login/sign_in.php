53aef72eb9c66104d8cc20a3dcc89bbb66a52e37
Auri Gabriel <ririmello.a@gmail.com>
2026-02-24 15:21:18 -0300

    Refactor: Enhance sign-in and sign-up forms with consistent card structure

--- application/views/pages/login/sign_in.php
+++ application/views/pages/login/sign_in.php
@@ -3,22 +3,22 @@
 		<div class="card">
 			<div class="card-header">
 				<h4>Sign In</h4>
-			<div class="container py-5">
-				<!-- Sign in form -->
 			</div>
-				<div class="form-group">
-					<label for="InputEmail1">Email address</label>
-					<input type="email" name="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp"
-						   placeholder="Enter email" required>
-					<small id="emailHelp" class="form-text text-muted">Enter your login information.</small>
-				</div>
-				<div class="form-group">
-					<label for="InputPassword">Password</label>
-					<input type="password" name="password" class="form-control" id="InputPassword"
-						   placeholder="Password" required>
-				</div>
-				<button type="submit" class="btn btn-success">Sign In</button>
-				</form>
+			<div class="card-body">
+				<?php echo form_open('Login_Controller/log_into', array('class' => 'form-signin')); ?>
+					<div class="form-group">
+						<label for="InputEmail1">Email address</label>
+						<input type="email" name="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp"
+							   placeholder="Enter email" required>
+						<small id="emailHelp" class="form-text text-muted">Enter your login information.</small>
+					</div>
+					<div class="form-group">
+						<label for="InputPassword">Password</label>
+						<input type="password" name="password" class="form-control" id="InputPassword"
+							   placeholder="Password" required>
+					</div>
+					<button type="submit" class="btn btn-success">Sign In</button>
+				<?php echo form_close(); ?>
 			</div>
 		</div>
 	</div>
--- application/views/pages/login/sign_up.php
+++ application/views/pages/login/sign_up.php
@@ -3,25 +3,25 @@
 		<div class="card">
 			<div class="card-header">
 				<h4>Join Thoth</h4>
-			<div class="container py-5">
-				<!-- Sign up form -->
 			</div>
-				<div class="form-group">
-					<label for="name">Name</label>
-					<input type="text" class="form-control" name="name" id="name" placeholder="Your Name" required>
-				</div>
-				<div class="form-group">
-					<label for="InputEmail1">Email address</label>
-					<input type="email" name="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp"
-						   placeholder="Enter email" required>
-				</div>
-				<div class="form-group">
-					<label for="InputPassword">Password</label>
-					<input type="password" name="password" class="form-control" id=InputPassword"
-						   placeholder="Password" required>
-				</div>
-				<button type="submit" class="btn btn-success">Create a new account</button>
-				</form>
+			<div class="card-body">
+				<?php echo form_open('Login_Controller/log_up', array('class' => 'form-signup')); ?>
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" class="form-control" name="name" id="name" placeholder="Your Name" required>
+					</div>
+					<div class="form-group">
+						<label for="InputEmail1">Email address</label>
+						<input type="email" name="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp"
+							   placeholder="Enter email" required>
+					</div>
+					<div class="form-group">
+						<label for="InputPassword">Password</label>
+						<input type="password" name="password" class="form-control" id="InputPassword"
+							   placeholder="Password" required>
+					</div>
+					<button type="submit" class="btn btn-success">Create a new account</button>
+				<?php echo form_close(); ?>
 			</div>
 		</div>
 	</div>
