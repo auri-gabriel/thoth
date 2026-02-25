b215d76d257414fa4decbe60284348cc29426065
Auri Gabriel <ririmello.a@gmail.com>
2026-02-25 16:24:28 -0300

    Refactor: Update tab navigation to Bootstrap 5 and enhance tab pane structure for improved accessibility

--- application/views/pages/project/reporting/reporting.php
+++ application/views/pages/project/reporting/reporting.php
@@ -4,19 +4,19 @@
 		<?php $this->load->view('pages/project/reporting/partials/tab_nav'); ?>
 		<div class="card-body">
 			<div class="tab-content">
-				<div class="tab-pane container-fluid active" id="tab_overview">
+				<div class="tab-pane fade show active container-fluid" id="tab_overview" role="tabpanel" aria-labelledby="tab_overview-tab">
 					<?php $this->load->view('pages/project/reporting/tabs/tab_overview'); ?>
 				</div>
-				<div class="tab-pane container-fluid" id="tab_import">
+				<div class="tab-pane fade container-fluid" id="tab_import" role="tabpanel" aria-labelledby="tab_import-tab">
 					<?php $this->load->view('pages/project/reporting/tabs/tab_import'); ?>
 				</div>
-				<div class="tab-pane container-fluid" id="tab_selection">
+				<div class="tab-pane fade container-fluid" id="tab_selection" role="tabpanel" aria-labelledby="tab_selection-tab">
 					<?php $this->load->view('pages/project/reporting/tabs/tab_selection'); ?>
 				</div>
-				<div class="tab-pane container-fluid" id="tab_quality">
+				<div class="tab-pane fade container-fluid" id="tab_quality" role="tabpanel" aria-labelledby="tab_quality-tab">
 					<?php $this->load->view('pages/project/reporting/tabs/tab_quality'); ?>
 				</div>
-				<div class="tab-pane container-fluid" id="tab_extraction">
+				<div class="tab-pane fade container-fluid" id="tab_extraction" role="tabpanel" aria-labelledby="tab_extraction-tab">
 					<?php $this->load->view('pages/project/reporting/tabs/tab_extraction'); ?>
 				</div>
 			</div>
