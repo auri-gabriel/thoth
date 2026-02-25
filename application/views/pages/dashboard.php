3f4464570990f90be639c5ba30ec88f9a76471ba
Auri Gabriel <ririmello.a@gmail.com>
2026-02-25 09:08:16 -0300

    Refactor: Revamp dashboard layout with enhanced styling and improved project display

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -1,46 +1,68 @@
-<div class="card">
-	<div class="card-header">
-		<h4>My Projects</h4>
+
+<section class="dashboard-section py-4">
 	<div class="container">
-		<!-- Dashboard content -->
+		<div class="row mb-4">
+			<div class="col-12 text-center">
+				<h2 class="fw-bold">My Projects</h2>
+				<hr class="my-3">
+				<a href="<?= base_url('new_project'); ?>" class="btn btn-success new-project mb-3">
+					<span class="fas fa-folder-plus"></span> Create New Project
+				</a>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-12 col-lg-10">
+				<div class="card shadow-sm">
+					<div class="card-body p-0">
+						<table class="table table-hover mb-0">
+							<thead class="table-light">
+								<tr>
+									<th>Title</th>
+									<th>Created By</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($projects as $project) { ?>
+									<tr>
+										<td class="align-middle">
+											<span class="fw-semibold text-dark">
+												<?= htmlspecialchars($project['project']->get_title()); ?>
+											</span>
+										</td>
+										<td class="align-middle">
+											<span class="badge bg-secondary">
+												<?= htmlspecialchars($project['project']->get_created_by()); ?>
+											</span>
+										</td>
+										<td class="align-middle">
+											<div class="d-flex flex-wrap gap-2">
+												<a href="<?= base_url('open/' . $project['project']->get_id()); ?>" class="btn btn-success btn-sm">
+													<span class="fas fa-folder-open"></span> Open
+												</a>
+												<?php if ($project['level'] == 1) { ?>
+													<a href="<?= base_url('edit/' . $project['project']->get_id()); ?>" class="btn btn-warning btn-sm">
+														<span class="fas fa-edit"></span> Edit
+													</a>
+													<a href="<?= base_url('add_research/' . $project['project']->get_id()); ?>" class="btn btn-info btn-sm">
+														<span class="fas fa-users-cog"></span> Add
+													</a>
+													<button type="button"
+														onclick="delete_project(<?= $project['project']->get_id() ?>,$(this).closest('tr'))"
+														class="btn btn-danger btn-sm">
+														<span class="fas fa-trash-alt"></span> Delete
+													</button>
+												<?php } ?>
+											</div>
+										</td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-			<thead>
-			<a href="<?= base_url('new_project'); ?>" class="btn btn-success new-project"><span
-					class="fas fa-folder-plus"></span> Create New Project</a>
-			<tr>
-				<th scope="col">Title</th>
-				<th scope="col">Created By</th>
-				<th scope="col">Actions</th>
-			</tr>
-			</thead>
-			<tbody>
-			<?php
-			foreach ($projects as $project) {
-				?>
-				<tr>
-					<td><?= $project['project']->get_title(); ?></td>
-					<td><?= $project['project']->get_created_by(); ?></td>
-					<td>
-						<a href="<?= base_url('open/' . $project['project']->get_id()); ?>"
-						   class="btn btn-outline-success opt"><span class="fas fa-folder-open"></span> Open</a>
-						<?php if ($project['level'] == 1) { ?>
-							<a href="<?= base_url('edit/' . $project['project']->get_id()); ?>"
-							   class="btn btn-outline-warning opt"><span class="fas fa-edit"></span> Edit</a>
-							<a href="<?= base_url('add_research/' . $project['project']->get_id()); ?>"
-							   class="btn btn-outline-info opt"><span class="fas fa-users-cog"></span> Add</a>
-							<button type="button"
-									onclick="delete_project(<?= $project['project']->get_id() ?>,$(this).parents('tr'))"
-									class="btn btn-outline-danger opt"><span
-									class="fas fa-trash-alt"></span> Delete
-							</button>
-						<?php } ?>
-					</td>
-				</tr>
-				<?php
-			}
-			?>
-			</tbody>
-		</table>
-	</div>
-</div>
+</section>
 
