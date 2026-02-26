882957231c1e82ce8fe6f3aa0c922e248c34f8a7
Auri Gabriel <ririmello.a@gmail.com>
2026-02-26 18:15:11 -0300

    refactor: standardize exit condition for BASEPATH definition

--- application/controllers/Home_Controller.php
+++ application/controllers/Home_Controller.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require_once APPPATH . 'controllers/Pattern_Controller.php';
 
@@ -10,6 +10,11 @@ class Home_Controller extends Pattern_Controller
 	 */
 	public function index()
 	{
+		if ($this->session->logged_in) {
+			redirect(base_url('dashboard'));
+			return;
+		}
+
 		load_templates('home', null);
 	}
 }
