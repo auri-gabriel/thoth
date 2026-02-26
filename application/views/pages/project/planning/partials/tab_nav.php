0f776c880ee272924731b7a764e361d5203847b9
Auri Gabriel <ririmello.a@gmail.com>
2026-02-26 14:51:30 -0300

    feat: update planning tab nav

--- application/views/pages/project/planning/partials/tab_nav.php
+++ application/views/pages/project/planning/partials/tab_nav.php
@@ -1,51 +1,51 @@
 <nav class="planning-tabs-nav mb-4 shadow bg-white p-2" aria-label="Planning tabs">
 	<ul class="nav nav-pills nav-justified flex-nowrap overflow-auto modern-tabs" role="tablist" style="white-space:nowrap;">
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link active modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_overall" aria-label="Overall information">
 				<span class="icon-circle mb-1"><i class="fas fa-info-circle fa-lg"></i></span>
-				   <span class="tab-label">Overall<br>information</span>
+				<span class="tab-label">Overall<br>information</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_research" aria-label="Research Questions">
 				<span class="icon-circle mb-1"><i class="fas fa-graduation-cap fa-lg"></i></span>
-				   <span class="tab-label">Research<br>Questions</span>
+				<span class="tab-label">Research<br>Questions</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_databases" aria-label="Data Bases">
 				<span class="icon-circle mb-1"><i class="fas fa-database fa-lg"></i></span>
-				   <span class="tab-label">Data<br>Bases</span>
+				<span class="tab-label">Data<br>Bases</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_search_string" aria-label="Search String">
 				<span class="icon-circle mb-1"><i class="fas fa-search-plus fa-lg"></i></span>
-				   <span class="tab-label">Search<br>String</span>
+				<span class="tab-label">Search<br>String</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_search_strategy" aria-label="Search Strategy">
 				<span class="icon-circle mb-1"><i class="fas fa-chess fa-lg"></i></span>
-				   <span class="tab-label">Search<br>Strategy</span>
+				<span class="tab-label">Search<br>Strategy</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_criteria" aria-label="Criteria">
 				<span class="icon-circle mb-1"><i class="fas fa-check-double fa-lg"></i></span>
 				<span class="tab-label">Criteria</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_quality" aria-label="Quality Assessment">
 				<span class="icon-circle mb-1"><i class="far fa-star fa-lg"></i></span>
-				   <span class="tab-label">Quality<br>Assessment</span>
+				<span class="tab-label">Quality<br>Assessment</span>
 			</a>
 		</li>
-		<li class="nav-item" style="min-width:140px;">
+		<li class="nav-item" style="min-width:140px; min-height:140px;">
 			<a data-bs-toggle="pill" class="nav-link modern-tab d-flex flex-column align-items-center justify-content-center py-2 px-3 fw-semibold" href="#tab_data" aria-label="Data Extraction">
 				<span class="icon-circle mb-1"><i class="fas fa-table fa-lg"></i></span>
-				   <span class="tab-label">Data<br>Extraction</span>
+				<span class="tab-label">Data<br>Extraction</span>
 			</a>
 		</li>
 	</ul>
