69d31096868350556521d0277ad7b307539912f0
Auri Gabriel <ririmello.a@gmail.com>
2026-02-27 16:33:29 -0300

    refactor: replace fetch with jQuery AJAX for Wikipedia API calls in getRelatedTerms function

--- assets/js/terms.js
+++ assets/js/terms.js
@@ -1,61 +1,74 @@
 async function getRelatedTerms(term) {
 	/* key_words - async function - consulta a wikipedia por termos similares ao pesquisado
-	
+
 	TODO: salvar no banco os resultados para evitar novas consultas e agilizar a busca
-	
+
 	args - string term - termo a ser pesquisado
-	
+
 	return - string array - termos relacionados a pesquisa
 	*/
 
 	if (!term) return null;
 
-	const conf = { method: "GET" };
-	let request, data;
+	let data;
 
-	request = await fetch(
-		"https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=" +
-			term +
-			"&srlimit=1&format=json",
-		conf,
-	);
-
-	data = await request.json();
+	// 1. Search for the pageid
+	data = await $.ajax({
+		url: "https://en.wikipedia.org/w/api.php",
+		method: "GET",
+		dataType: "json",
+		data: {
+			origin: "*",
+			action: "query",
+			list: "search",
+			srsearch: term,
+			srlimit: 1,
+			format: "json",
+		},
+	});
 
 	const pageid = data.query.search[0].pageid;
 
-	request = await fetch(
-		"https://en.wikipedia.org/w/api.php?origin=*&action=parse&prop=sections&format=json&pageid=" +
-			pageid,
-		conf,
-	);
+	// 2. Get sections for the page
+	data = await $.ajax({
+		url: "https://en.wikipedia.org/w/api.php",
+		method: "GET",
+		dataType: "json",
+		data: {
+			origin: "*",
+			action: "parse",
+			prop: "sections",
+			format: "json",
+			pageid: pageid,
+		},
+	});
 
-	data = await request.json();
-
-	sections = data.parse.sections;
-
+	const sections = data.parse.sections;
 	let index = -1;
-
 	for (let i = 0; i < sections.length; i++) {
 		if (sections[i].anchor.match(/See\_also/)) {
 			index = sections[i].index;
 			break;
 		}
 	}
-
 	if (index < 0) return null;
 
-	request = await fetch(
-		"https://en.wikipedia.org/w/api.php?origin=*&action=parse&prop=links&format=json&pageid=" +
-			pageid +
-			"&section=" +
-			index,
-		conf,
-	);
+	// 3. Get links from the "See also" section
+	data = await $.ajax({
+		url: "https://en.wikipedia.org/w/api.php",
+		method: "GET",
+		dataType: "json",
+		data: {
+			origin: "*",
+			action: "parse",
+			prop: "links",
+			format: "json",
+			pageid: pageid,
+			section: index,
+		},
+	});
 
-	data = await request.json();
-
-	terms = data.parse.links.map((i) => i["*"].replace(/.*\:/, ""));
+	const terms = data.parse.links.map((i) => i["*"].replace(/.*\:/, ""));
 
 	SwalAdapter.fire({
 		title: "Terms Retrieved",
