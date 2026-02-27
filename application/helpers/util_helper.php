ad342801c3ebd73845b5b841d786d6f5aaca81e3
Auri Gabriel <ririmello.a@gmail.com>
2026-02-26 21:50:26 -0300

    feat: implement navbar template with responsive design and user authentication links

--- application/helpers/util_helper.php
+++ application/helpers/util_helper.php
@@ -1,12 +1,12 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 
-if ( ! function_exists('load_templates'))
-{
-	function load_templates($var,$data){
-		$ci=& get_instance();
+if (! function_exists('load_templates')) {
+	function load_templates($var, $data)
+	{
+		$ci = &get_instance();
 		$ci->load->view('templates/header');
-		$ci->load->view('templates/menu');
-		$ci->load->view($var,$data);
+		$ci->load->view('templates/navbar');
+		$ci->load->view($var, $data);
 		$ci->load->view('templates/footer');
 	}
 }
