12f281de49a3197942f8bdb3f3ec6ca18df8e4e9
Auri Gabriel <ririmello.a@gmail.com>
2026-02-28 00:41:18 -0300

    feat: implement export functionality with Twig template for project data

--- application/controllers/Project_Controller.php
+++ application/controllers/Project_Controller.php
@@ -210,7 +210,10 @@ class Project_Controller extends Pattern_Controller
 
 			$data['project'] = $this->Project_Model->get_project_export($id);
 
-			$this->load_views('pages/project/export', $data);
+			$this->render(
+				'pages/project/export.twig',
+				['data' => $data]
+			);
 		} catch (Exception $e) {
 			$this->session->set_flashdata('error', $e->getMessage());
 			redirect(base_url());
