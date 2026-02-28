ebfc3853314e589b174e4741286e0a1876a104a0
Auri Gabriel <ririmello.a@gmail.com>
2026-02-28 05:27:09 -0300

    fix: login model

--- application/models/Login_Model.php
+++ application/models/Login_Model.php
@@ -1,7 +1,63 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login_Model extends CI_Model
+{
+
+	public function check_email_unique($email)
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where('email', $email);
+		$query = $this->db->count_all_results();
+
+		if ($query > 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public function new_user($email, $password, $name)
+	{
+		$now = new DateTime();
+		$now->getTimestamp();
+
+		$data = array(
+			'email' => $email,
+			'name' => $name,
+			'password' => $password,
+			'created_at' => $now->format('Y-m-d H:i:s')
+		);
+
+		$this->db->insert('user', $data);
+
+		$user = new User();
+		$user->set_name($name);
+		$user->set_email($email);
+
+		return $user;
+	}
+
+	public function check_login($email, $password)
+	{
+		$user = new User();
+		$password = md5($password);
+
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where(array('email =' => $email, 'password' => $password));
+		$query = $this->db->get();
+
+		foreach ($query->result() as $row) {
+			$user->set_name($row->name);
+			$user->set_email($row->email);
+			return $user;
+		}
+
+		return null;
+	}
+
 	// Find user by email
 	public function find_by_email($email)
 	{
@@ -70,60 +126,4 @@ class Login_Model extends CI_Model
 		$this->db->where('token', $token);
 		$this->db->update('password_resets', array('is_used' => 1));
 	}
-{
-
-	public function check_email_unique($email)
-	{
-		$this->db->select('*');
-		$this->db->from('user');
-		$this->db->where('email', $email);
-		$query = $this->db->count_all_results();
-
-		if ($query > 0) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public function new_user($email, $password, $name)
-	{
-		$now = new DateTime();
-		$now->getTimestamp();
-
-		$data = array(
-			'email' => $email,
-			'name' => $name,
-			'password' => $password,
-			'created_at' => $now->format('Y-m-d H:i:s')
-		);
-
-		$this->db->insert('user', $data);
-
-		$user = new User();
-		$user->set_name($name);
-		$user->set_email($email);
-
-		return $user;
-	}
-
-	public function check_login($email, $password)
-	{
-		$user = new User();
-		$password = md5($password);
-
-		$this->db->select('*');
-		$this->db->from('user');
-		$this->db->where(array('email =' => $email, 'password' => $password));
-		$query = $this->db->get();
-
-		foreach ($query->result() as $row) {
-			$user->set_name($row->name);
-			$user->set_email($row->email);
-			return $user;
-		}
-
-		return null;
-	}
-
 }
