9b76609565598e30ed0e971a96601cbe8e832a86
Auri Gabriel <ririmello.a@gmail.com>
2026-02-28 06:40:14 -0300

    feat: add email configuration and MailHog service to docker-compose

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'localhost';
+$config['smtp_port'] = 1025; // MailHog default
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['mailtype'] = 'text';
+$config['charset']  = 'utf-8';
