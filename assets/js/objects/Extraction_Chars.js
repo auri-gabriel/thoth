50c70bb0c69fd2a7feb47a4d37f8c6991d545f9e
Auri Gabriel <ririmello.a@gmail.com>
2026-03-01 18:08:15 -0300

    feat: enhance reporting tabs and extraction charts

--- assets/js/objects/Extraction_Chars.js
+++ assets/js/objects/Extraction_Chars.js
@@ -1,25 +1,33 @@
 class Extraction_Chars {
-	constructor(id, type, data) {
+	constructor(id, type, data, parentId = "tab_data_extraction_charts") {
 		this.id = id;
 		this.type = type;
 		this.data = data;
+		this.parentId = parentId;
 		this.char = null;
 	}
 
 	show() {
-		let parent = document.getElementById('tab_ex');
+		let parent = document.getElementById(this.parentId);
+		if (!parent) {
+			return;
+		}
 
-		let card = document.createElement('div');
+		const chartId = `${this.id}_${this.type.replace(/\s+/g, "_").toLowerCase()}`;
+		if (document.getElementById(chartId)) {
+			return;
+		}
+
+		let card = document.createElement("div");
 		card.classList.add("card");
 
-
-		let card_body = document.createElement('div');
+		let card_body = document.createElement("div");
 		card_body.classList.add("card-body");
 
-		let div_char = document.createElement('div');
-		div_char.id = this.id;
+		let div_char = document.createElement("div");
+		div_char.id = chartId;
 
-		let br = document.createElement('br');
+		let br = document.createElement("br");
 
 		card_body.appendChild(div_char);
 		card.appendChild(card_body);
@@ -28,57 +36,63 @@ class Extraction_Chars {
 
 		switch (this.type) {
 			case "Multiple Choice List":
-				this.char = Highcharts.chart(this.id, {
-					series: [{
-						type: 'venn',
-						name: 'Answers to the question ' + this.id,
-						data: this.data
-					}],
+				this.char = Highcharts.chart(chartId, {
+					series: [
+						{
+							type: "venn",
+							name: "Answers to the question " + this.id,
+							data: this.data,
+						},
+					],
 					title: {
-						text: 'Answers to the question ' + this.id
-					}
+						text: "Answers to the question " + this.id,
+					},
 				});
 
 				console.log(this.data);
 				break;
 			case "Pick One List":
-				this.char = Highcharts.chart(this.id, {
+				this.char = Highcharts.chart(chartId, {
 					chart: {
 						plotBackgroundColor: null,
 						plotBorderWidth: null,
 						plotShadow: false,
-						type: 'pie'
+						type: "pie",
 					},
 					title: {
-						text: 'Answers to the question ' + this.id
+						text: "Answers to the question " + this.id,
 					},
 					tooltip: {
-						pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b> ({point.y})'
+						pointFormat:
+							"{series.name}: <b>{point.percentage:.1f}%</b> ({point.y})",
 					},
 					plotOptions: {
 						column: {
-							colorByPoint: true
+							colorByPoint: true,
 						},
 						pie: {
 							allowPointSelect: true,
-							cursor: 'pointer',
+							cursor: "pointer",
 							dataLabels: {
 								enabled: true,
-								format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+								format: "<b>{point.name}</b>: {point.percentage:.1f} %",
 								style: {
-									color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-								}
-							}
-						}
+									color:
+										(Highcharts.theme && Highcharts.theme.contrastTextColor) ||
+										"black",
+								},
+							},
+						},
 					},
-					series: [{
-						name: 'Brands',
-						colorByPoint: true,
-						data: this.data
-					}]
+					series: [
+						{
+							name: "Brands",
+							colorByPoint: true,
+							data: this.data,
+						},
+					],
 				});
 				break;
 		}
 	}
-
 }
